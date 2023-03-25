b917558068682d6dd89c4d6dd46bd1dbb013ffe7
Oussama-hamdi <ohamdi776@gmail.com>
2023-03-25 05:06:13 -0700

    testing code

--- _printf.c
+++ _printf.c
@@ -1,22 +1,81 @@
 #include "main.h"
 
 /**
- * _printf - print a formated string
- * @format: A string containing all the desired characters
- * Return: length of the printed characters
+ * _print_char - writes a character to stdout
+ * @args: argument list
+ *
+ * Return: 1
  */
+int _print_char(va_list args)
+{
+	char c = va_arg(args, int);
 
+	putchar(c);
+
+	return (1);
+}
+
+/**
+ * _print_string - writes a string to stdout
+ * @args: argument list
+ *
+ * Return: number of characters printed
+ */
+int _print_string(va_list args)
+{
+	char *str = va_arg(args, char*);
+	int count = 0;
+
+	while (*str)
+	{
+		putchar(*str);
+		str++;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * _printf - prints formatted output to stdout
+ * @format: format string
+ *
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-	int count;
-
 	va_list args;
 
-	if (!format)
-		return (1);
-
 	va_start(args, format);
-	count = vprintf(format, args);
+	int count = 0;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					count += _print_char(args);
+					break;
+				case 's':
+					count += _print_string(args);
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+
 	va_end(args);
 	return (count);
 }
