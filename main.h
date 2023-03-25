dd65f424eea94b5f7221543d591c162ad266bd7e
Oussama-hamdi <ohamdi776@gmail.com>
2023-03-25 04:31:36 -0700

    fixed code

--- main.h
+++ main.h
@@ -6,22 +6,7 @@
 #include <limits.h>
 #include <stdarg.h>
 
-
-/**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function to print
- */
-typedef struct print
-{
-	char *t;
-	int (*f)(va_list);
-} print_t;
-
-
-
 int _printf(const char *format, ...);
 int _putchar(char c);
-int parser(const char *format, print_t p[], va_list arg_list);
 
 #endif /* MAIN_H */
