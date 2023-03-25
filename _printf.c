5aa423ade1f3e4c91268e461f793e61fc2cc33aa
Oussama-hamdi <ohamdi776@gmail.com>
2023-03-25 04:08:45 -0700

    testing _printf

---
+++ _printf.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _printf - print a formated string
+ * @format: A string containing all the desired characters
+ * Return: length of the printed characters
+ */
+
+int _printf(const char *format, ...)
+{
+	int count;
+	va_list args;
+
+	if (!format)
+		return (1);
+	
+	va_start(args, format);
+	count = vprintf(format, args);
+	va_end(args);
+	return count;
+}
